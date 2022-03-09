539eef2fafb8aef7d430250a79c613b72343eee6
khalil <khalillakhdharatc@gmail.com>
2022-03-09 11:49:29 +0100

    Initial commit

---
+++ src/main/java/com/duranco/ProductsListingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.duranco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsListingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsListingSpringApplication.class, args);
+	}
+
+}
